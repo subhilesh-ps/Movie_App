f79546cf2727cd4d520b645ca34277a4d66cf149
subhilesh-ps <subhileshps96@gmail.com>
2024-08-06 17:25:06 +0530

    navebar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import { useState } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+import Home from "./pages/home/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <div className="App">
+        <Router>
+          <Header />
+          <Routes>
+            <Route index element={<Home />}></Route>
+            <Route path="movie/:id" element={<h1>Movie detail page</h1>}></Route>
+            <Route path="movies/:type" element={<h1>Movie list page</h1>}></Route>
+            <Route path="/*" element={<h1>Error page</h1>}></Route>
+          </Routes>
+        </Router>
+      </div>
+    </>
+  );
 }
 
 export default App;
